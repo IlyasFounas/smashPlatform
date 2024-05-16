b40cfa0b41ddecdf73bfa93ffc925cca1dd6ec8b
try hard <ilyas22022003@gmail.com>
2024-05-16 08:19:14 +0200

    we can move right or left and even jump

---
+++ jeu2D/Script/worldEnv.gd
@@ -0,0 +1,18 @@
+extends Node2D
+@onready var OBSTACLE = preload("res://Scenes/obstacles.tscn")
+var compteur = 0
+var rng = RandomNumberGenerator.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#put the screen on full mode
+	get_viewport().get_window().mode = get_viewport().get_window().MODE_EXCLUSIVE_FULLSCREEN
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if compteur < 20:
+		var obstacle = OBSTACLE.instantiate()
+		obstacle.position = Vector2(rng.randf_range(0, get_viewport_rect().size.x),rng.randf_range(0, get_viewport_rect().size.y))
+		add_child(obstacle)
+	compteur = compteur + 1
