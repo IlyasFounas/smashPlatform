5f6777c66d3acd3c26914f0b87daa3ca67ad9d63
try hard <ilyas22022003@gmail.com>
2024-05-29 23:01:16 +0200

    the peer to peer server work, and we can almost see the other user fireball

--- jeu2D/Script/user.gd
+++ jeu2D/Script/user.gd
@@ -8,170 +8,178 @@ const JUMP_VELOCITY = -400.0
 @onready var SHIELD = preload("res://Scenes/shield.tscn")
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var cam = $Camera3D
 var compteurJump = 0
 var compteurMunition = 0
 var userLeft = false
 var life = 3
 var compteurLife = 0
 
+
 func _on_ready():
+	cam.current = is_multiplayer_authority()
 	get_child(1).visible = false
 	get_child(2).visible = false
 	get_child(3).visible = false
 	get_child(4).visible = false
 	get_child(5).visible = false
 
+func _enter_tree():
+	set_multiplayer_authority(name.to_int())
+
 func _physics_process(delta):
-	get_parent().get_child(15).get_child(3).get_child(0).text = str(compteurLife)
-	if life > 0:
-		if position.x > 1920:
-			life = life -1 
-			position = Vector2(300,300)
-		if position.y > 1080:
-			life = life -1
-			position = Vector2(300,300)
-		if position.y < 0 :
-			life = life - 1
-			position = Vector2(300,300)
+	if is_multiplayer_authority():
+		#get_parent().$screen_mario.get_child(3).get_child(0).text = str(compteurLife)
+		if life > 0:
+			if position.x > 1920:
+				life = life -1 
+				position = Vector2(300,300)
+			if position.y > 1080:
+				life = life -1
+				position = Vector2(300,300)
+			if position.y < 0 :
+				life = life - 1
+				position = Vector2(300,300)
 			
-	if life == 0:
-		get_parent().get_child(14).get_child(3).get_child(0).text = "Death"
-	#the user can jump 3 time in row if he want
-	if compteurJump < 2:
-		# Handle Jump.
-		if Input.is_action_just_pressed("ui_accept"):
-			if !Input.is_action_just_pressed("ui_q"):
-				if userLeft :
+		if life == 0:
+			get_parent().get_child(14).get_child(3).get_child(0).text = "Death"
+		#the user can jump 3 time in row if he want
+		if compteurJump < 2:
+			# Handle Jump.
+			if Input.is_action_just_pressed("ui_accept"):
+				if !Input.is_action_just_pressed("ui_q"):
+					if userLeft :
+						get_child(1).visible = false
+						get_child(2).visible = false
+						get_child(3).visible = true
+						get_child(4).visible = false
+						get_child(5).visible = false
+					else : 
+						get_child(1).visible = false
+						get_child(2).visible = true
+						get_child(3).visible = false
+						get_child(4).visible = false
+						get_child(5).visible = false
+				elif !Input.is_action_just_pressed("ui_d"):
+					if userLeft :
+						get_child(1).visible = false
+						get_child(2).visible = false
+						get_child(3).visible = true
+						get_child(4).visible = false
+						get_child(5).visible = false
+					else : 
+						get_child(1).visible = false
+						get_child(2).visible = true
+						get_child(3).visible = false
+						get_child(4).visible = false
+						get_child(5).visible = false
+				velocity.y = JUMP_VELOCITY
+				compteurJump = compteurJump + 1
+			if Input.is_action_just_pressed("ui_z"):
+				if !Input.is_action_just_pressed("ui_q"):
 					get_child(1).visible = false
 					get_child(2).visible = false
 					get_child(3).visible = true
 					get_child(4).visible = false
 					get_child(5).visible = false
-				else : 
-					get_child(1).visible = false
-					get_child(2).visible = true
-					get_child(3).visible = false
-					get_child(4).visible = false
-					get_child(5).visible = false
-			elif !Input.is_action_just_pressed("ui_d"):
-				if userLeft :
+				elif !Input.is_action_just_pressed("ui_d"):
 					get_child(1).visible = false
 					get_child(2).visible = false
 					get_child(3).visible = true
 					get_child(4).visible = false
 					get_child(5).visible = false
-				else : 
-					get_child(1).visible = false
-					get_child(2).visible = true
-					get_child(3).visible = false
-					get_child(4).visible = false
-					get_child(5).visible = false
-			velocity.y = JUMP_VELOCITY
-			compteurJump = compteurJump + 1
-		if Input.is_action_just_pressed("ui_z"):
-			if !Input.is_action_just_pressed("ui_q"):
-				get_child(1).visible = false
-				get_child(2).visible = false
-				get_child(3).visible = true
-				get_child(4).visible = false
-				get_child(5).visible = false
-			elif !Input.is_action_just_pressed("ui_d"):
-				get_child(1).visible = false
-				get_child(2).visible = false
-				get_child(3).visible = true
-				get_child(4).visible = false
-				get_child(5).visible = false
-			velocity.y = JUMP_VELOCITY
-			compteurJump = compteurJump + 1
+				velocity.y = JUMP_VELOCITY
+				compteurJump = compteurJump + 1
 
-			
-	#when the user is on a surface/background/enemie he gain 3 jumpd 
-	if is_on_floor(): 
-		compteurJump = 0
+				
+		#when the user is on a surface/background/enemie he gain 3 jumpd 
+		if is_on_floor(): 
+			compteurJump = 0
 
-	#the user go down if he press s
-	if Input.is_action_just_pressed('ui_s'):
-		velocity.y += gravity * 1.2
-		get_child(1).visible = false
-		get_child(2).visible = false
-		get_child(3).visible = false
-		get_child(4).visible = false
-		get_child(5).visible = true
-		if is_on_floor():
+		#the user go down if he press s
+		if Input.is_action_just_pressed('ui_s'):
+			velocity.y += gravity * 1.2
 			get_child(1).visible = false
 			get_child(2).visible = false
-			get_child(3).visible = true
+			get_child(3).visible = false
 			get_child(4).visible = false
-			get_child(5).visible = false
+			get_child(5).visible = true
+			if is_on_floor():
+				get_child(1).visible = false
+				get_child(2).visible = false
+				get_child(3).visible = true
+				get_child(4).visible = false
+				get_child(5).visible = false
+			
+		# Add the gravity.
+		if not is_on_floor():
+			velocity.y += gravity * delta
+
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction = Input.get_axis("ui_q", "ui_d")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		move_and_slide()
 		
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_q", "ui_d")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	move_and_slide()
-	
-	if Input.is_action_just_pressed('ui_q'):
-		userLeft = true
-		if is_on_floor():
-			get_child(1).visible = false
-			get_child(2).visible = false
-			get_child(3).visible = false
-			get_child(4).visible = true
-			get_child(5).visible = false
-		if !is_on_floor(): 
-			get_child(1).visible = false
-			get_child(2).visible = false
-			get_child(3).visible = true
-			get_child(4).visible = false
-			get_child(5).visible = false
-	
-	if Input.is_action_just_pressed('ui_d'):
-		userLeft = false
-		if is_on_floor():
-			get_child(1).visible = true
-			get_child(2).visible = false
-			get_child(3).visible = false
-			get_child(4).visible = false
-			get_child(5).visible = false
-		if !is_on_floor(): 
-			get_child(1).visible = false
-			get_child(2).visible = true
-			get_child(3).visible = false
-			get_child(4).visible = false
-			get_child(5).visible = false
+		if Input.is_action_just_pressed('ui_q'):
+			userLeft = true
+			if is_on_floor():
+				get_child(1).visible = false
+				get_child(2).visible = false
+				get_child(3).visible = false
+				get_child(4).visible = true
+				get_child(5).visible = false
+			if !is_on_floor(): 
+				get_child(1).visible = false
+				get_child(2).visible = false
+				get_child(3).visible = true
+				get_child(4).visible = false
+				get_child(5).visible = false
+		
+		if Input.is_action_just_pressed('ui_d'):
+			userLeft = false
+			if is_on_floor():
+				get_child(1).visible = true
+				get_child(2).visible = false
+				get_child(3).visible = false
+				get_child(4).visible = false
+				get_child(5).visible = false
+			if !is_on_floor(): 
+				get_child(1).visible = false
+				get_child(2).visible = true
+				get_child(3).visible = false
+				get_child(4).visible = false
+				get_child(5).visible = false
 	
 func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == 1 && event.pressed == true:
-			if compteurMunition < 6:
-				var munition = MUNITION.instantiate()
-				if userLeft :
-					munition.position = position 
-					munition.position.x = munition.position.x - 50
-					munition.initialVelocity = Vector2(-1,0)
-					get_parent().add_child(munition)
-					compteurMunition = compteurMunition + 1
-				else :
-					munition.position = position
-					munition.position.x = munition.position.x + 50
-					munition.initialVelocity = Vector2(1,0)
-					get_parent().add_child(munition)
-					compteurMunition = compteurMunition + 1
+	if is_multiplayer_authority():
+		if event is InputEventMouseButton:
+			if event.button_index == 1 && event.pressed == true:
+				if compteurMunition < 6:
+					var munition = MUNITION.instantiate()
+					if userLeft :
+						munition.position = position 
+						munition.position.x = munition.position.x - 50
+						munition.initialVelocity = Vector2(-1,0)
+						get_parent().add_child(munition)
+						compteurMunition = compteurMunition + 1
+					else :
+						munition.position = position
+						munition.position.x = munition.position.x + 50
+						munition.initialVelocity = Vector2(1,0)
+						get_parent().add_child(munition)
+						compteurMunition = compteurMunition + 1
+					
+		elif event is InputEventKey:
+			match event.keycode:
+				70:
+					# F
+					if event.is_pressed():
+						var shield = SHIELD.instantiate()
+						shield.position = position
+						get_parent().add_child(shield)
 				
-	elif event is InputEventKey:
-		match event.keycode:
-			70:
-				# F
-				if event.is_pressed():
-					var shield = SHIELD.instantiate()
-					shield.position = position
-					get_parent().add_child(shield)
-			
 
--- jeu2D/Script/worldEnv.gd
+++ jeu2D/Script/worldEnv.gd
@@ -1,6 +1,9 @@
 extends Node2D
 @onready var OBSTACLE = preload("res://Scenes/obstacles.tscn")
 @onready var MUNITION_BOX = preload("res://Scenes/munitionBox.tscn")
+var peer = ENetMultiplayerPeer.new()
+@export var player_scene : PackedScene
+
 var compteur = 0
 var rng = RandomNumberGenerator.new()
 var compteurMunitionBox = 0
@@ -8,8 +11,8 @@ var compteurMunitionBox = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#put the screen on full mode
-	get_viewport().get_window().mode = get_viewport().get_window().MODE_EXCLUSIVE_FULLSCREEN
-
+	#get_viewport().get_window().mode = get_viewport().get_window().MODE_EXCLUSIVE_FULLSCREEN
+	pass
 
 # Called every frame. 'delta' is the elapsed time sincd e the previous frame.
 func _process(delta):
@@ -27,3 +30,34 @@ func _on_timer_timeout():
 		muntion_box.position = Vector2(rng.randf_range(400, 800),rng.randf_range(0, get_viewport_rect().size.y))
 		add_child(muntion_box)
 		compteurMunitionBox = compteurMunitionBox +1
+
+
+func _on_host_pressed():
+	peer.create_server(1027)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(add_player)
+	add_player()
+	$CanvasLayer.hide()
+
+func add_player(id = 1):
+	var player = player_scene.instantiate()
+	player.name = str(id)
+	call_deferred("add_child", player)
+
+
+func _on_join_pressed():
+	peer.create_client("127.0.0.1",1027)
+	multiplayer.multiplayer_peer = peer
+	$CanvasLayer.hide()
+	
+func exit_game(id):
+	multiplayer.peer_disconnected.connect(del_player)
+	del_player(id)
+	
+	
+func del_player(id):
+	rpc("_del_player",id)
+	
+@rpc("any_peer","call_local")
+func _del_player(id):
+	get_node(str(id)).queue_free()
